2fea76a914eeb07bb228e5a3358d920b87a17881
Peter Robinson <Peter.Robinson@softwire.com>
2019-07-19 15:00:48 +0100

    Added inheritance to API classes

--- API.js
+++ API.js
@@ -1,9 +1,34 @@
 const requestPromise = require("request-promise-native");
 
-module.exports.TflAPI = class tflAPI {
+
+class API {
+    constructor(url){
+        this.url = url;
+        this.baseQueries = {};
+    }
+    makeRequest(endpoint, queries) {
+        const options = {
+            uri: this.url + endpoint,
+            qs: {
+                ...this.baseQueries,
+                ...queries
+            },
+            headers: {
+                "User-Agent": "Request-Promise"
+            },
+            json: true // Automatically parses the JSON string in the response
+        };
+        return requestPromise(options);
+    }
+}
+
+module.exports.TflAPI = class tflAPI extends API{
     constructor(AppID, AppKey) {
-        this.AppID = AppID;
-        this.AppKey = AppKey;
+        super("https://api.tfl.gov.uk");
+        this.baseQueries = {
+            app_key: AppKey,
+            app_id: AppID
+        };
     }
     getBusTimesFromStop(stopCode) {
         const endpoint = `/StopPoint/${stopCode}/Arrivals`;
@@ -20,34 +45,12 @@ module.exports.TflAPI = class tflAPI {
         const endpoint = "/StopPoint";
         return this.makeRequest(endpoint, queries);
     }
-
-    makeRequest(endpoint, queries) {
-        const options = {
-            uri: "https://api.tfl.gov.uk" + endpoint,
-            qs: {
-                app_key: this.AppKey,
-                app_id: this.AppID,
-                ...queries
-            },
-            headers: {
-                "User-Agent": "Request-Promise"
-            },
-            json: true // Automatically parses the JSON string in the response
-        };
-        return requestPromise(options);
-    }
 };
 
-module.exports.LocationAPI = class locationAPI {
-    makeRequest(endpoint) {
-        const options = {
-            uri: "https://api.postcodes.io/postcodes" + endpoint,
-            headers: {
-                "User-Agent": "Request-Promise"
-            },
-            json: true // Automatically parses the JSON string in the response
-        };
-        return requestPromise(options);
+module.exports.LocationAPI = class locationAPI extends API{
+
+    constructor(){
+        super("https://api.postcodes.io/postcodes");
     }
 
     getCoordinates(postcode) {
